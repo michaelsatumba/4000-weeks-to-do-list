d5ee8ff9c52022c8a83ed011650ecca032ea8e96
michaelsatumba <mjsatumba@gmail.com>
2024-05-22 10:27:01 -0700

    Login functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
-import Image from "next/image";
+'use client';
+import { auth, googleProvider } from "../../utils/firebaseConfig";
+import { signInWithPopup } from "firebase/auth";
 
 export default function Home() {
+  const handleGoogleSignUp = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      // Handle successful login here (e.g., navigate to a different page)
+      console.log("User signed in successfully:", result.user);
+    } catch (error) {
+      console.error("Error signing in with Google:", error);
+      // Handle errors here (e.g., display an error message)
+    }
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="text-4xl font-bold mb-4 whitespace-nowrap">4000 weeks to do list</h1>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Sign In
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      onClick={handleGoogleSignUp}
+      >
+        Sign In with Google
       </button>
     </main>
   );
--- utils/firebaseConfig.ts
+++ utils/firebaseConfig.ts
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -21,6 +18,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Analytics
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
